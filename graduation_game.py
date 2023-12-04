d487c086f48969e3f348f416782db9c1ceaa9ada
성민서 <seongminseo1010@gmail.com>
2023-12-04 15:02:47 +0900

    장애물, 체력 화면 출력

--- graduation_game.py
+++ graduation_game.py
@@ -9,29 +9,38 @@ black = (0, 0, 0)
 screen_width = 1000
 screen_height = 800
 map_width = 4000
-map_height = 1000
+map_height = 800
 
-def runGame(screen, clock, player_image, background_stage1_1, background_stage1_2):
+def runGame(screen, clock, player_image, background_stage1, assignment_image, hp_images):
     # 플레이어 설정
-    player_width = 50
-    player_height = 50
-    player_x = screen_width // 2 - player_width // 2
-    player_y = screen_height - player_height - 10
+    player_width = 100
+    player_height = 100
+    player_x = 50
+    player_y = screen_height - player_height
     player_speed = 10
     jump_height = 20
+    player_hp = 3  # 플레이어의 초기 체력
 
     # 플레이어의 초기 속도 및 중력 설정
     player_velocity_y = 0
     gravity = 1.5
 
-    # 카메라 설정
+    # 장애물(과제) 설정
+    assignment_width = 100
+    assignment_height = 100
+    assignment_x = [1000, 2000, 3000]  # 3개의 장애물 생성
+    assignment_y = screen_height - assignment_height
+    assignment_speed = 20
+
+    # 카메라 위치 설정
     camera_x = 0
+
+    running = True
     
-    while True:
+    while running and player_hp > 0:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+                running = False
 
         keys = pygame.key.get_pressed()
 
@@ -40,7 +49,7 @@ def runGame(screen, clock, player_image, background_stage1_1, background_stage1_
             player_x -= player_speed
         if keys[pygame.K_RIGHT] and player_x < map_width - player_width:
             player_x += player_speed
-        if keys[pygame.K_SPACE] and player_y == screen_height - player_height - 10:
+        if keys[pygame.K_SPACE] and player_y == screen_height - 100:
             player_velocity_y = -jump_height
 
         # 중력 적용
@@ -48,26 +57,50 @@ def runGame(screen, clock, player_image, background_stage1_1, background_stage1_
         player_y += player_velocity_y
 
         # 바닥에 닿으면 중력 초기화
-        if player_y > screen_height - player_height - 10:
-            player_y = screen_height - player_height - 10
+        if player_y > screen_height - 100:
+            player_y = screen_height - 100
             player_velocity_y = 0
 
-        # 카메라 위치 조정
-        camera_x = player_x - screen_width // 2.5
+        # 카메라 위치 업데이트
+        camera_x = player_x - screen_width/2.5
 
         # 배경 그리기 (맵의 배경)
-        screen.fill(white)  # 화면을 흰색으로 지우기
-        screen.blit(background_stage1_1, (0 - camera_x - screen_width // 2, 0))
-        screen.blit(background_stage1_2, (screen_width - camera_x, 0))
+        screen.blit(background_stage1, (0 - camera_x - 400, 0))
 
-        # 맵을 카메라 위치에 따라 그리기
-        screen.blit(player_image, (player_x - camera_x, player_y - 50))
+        # 플레이어를 카메라 위치에 따라 그리기
+        screen.blit(player_image, (player_x - camera_x, player_y))
+
+        # 장애물 그리기
+        for i in range(3):
+            screen.blit(assignment_image, (assignment_x[i] - camera_x, assignment_y))
+
+            # 플레이어와 장애물 충돌 체크
+            if (
+                player_x < assignment_x[i] + assignment_width and
+                player_x + player_width > assignment_x[i] and
+                player_y < assignment_y + assignment_height and
+                player_y + player_height > assignment_y - 20  # 충돌 기준을 완화
+            ):
+                # 충돌 시 HP 감소 및 재위치 설정
+                player_hp -= 1
+                player_x = 50
+                player_y = screen_height - player_height
+
+                 
+        # HP 이미지 표시
+        if (player_hp >= 1): screen.blit(pygame.transform.scale(hp_images[player_hp], (100, 50)), (10, 10))
+
 
         # 화면 업데이트
         pygame.display.flip()
 
         # FPS 설정
         clock.tick(30)
+
+    # 게임 종료 메시지 출력
+    print("게임 종료")
+    pygame.quit()
+    sys.exit()
 
 def initGame():
     pygame.init()
@@ -79,10 +112,19 @@ def initGame():
     player_image = pygame.transform.scale(player_image, (100, 100))
 
     # 배경 이미지 로드
-    background_stage1_1 = pygame.image.load("송민학교1.png")
-    background_stage1_1 = pygame.transform.scale(background_stage1_1, (map_width, map_height))
-    background_stage1_2 = pygame.image.load("송민학교2.png")
-    background_stage1_2 = pygame.transform.scale(background_stage1_2, (map_width, map_height))
+    background_stage1 = pygame.image.load("송민학교1.png")
+    background_stage1 = pygame.transform.scale(background_stage1, (map_width + 1000, map_height))
+
+    # 장애물(과제) 이미지 로드
+    assignment_image = pygame.image.load("assignment_image.png")
+    assignment_image = pygame.transform.scale(assignment_image, (100, 100))
+
+    # HP 이미지 로드
+    hp_images = {
+        3: pygame.image.load("hp3.png").convert_alpha(),
+        2: pygame.image.load("hp2.png").convert_alpha(),
+        1: pygame.image.load("hp1.png").convert_alpha(),
+    }
 
     # 초기 배경 음악 로드 및 재생 (7초짜리)
     pygame.mixer.music.load("initial_music.mp3")
@@ -95,10 +137,10 @@ def initGame():
     # 초기 음악이 끝나면 다른 음악으로 교체 및 반복 재생
     pygame.mixer.music.load("loop_music.mp3")
     pygame.mixer.music.play(-1)
-
+    
     # 메인 루프
     clock = pygame.time.Clock()
-    runGame(screen, clock, player_image, background_stage1_1, background_stage1_2)
+    runGame(screen, clock, player_image, background_stage1, assignment_image, hp_images)
 
 if __name__ == '__main__':
     initGame()
