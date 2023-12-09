071c97c4fb18c8ca297b4a24d93a6cddd084dabe
성민서 <seongminseo1010@gmail.com>
2023-12-10 02:13:12 +0900

    맵 이어지기, 보스 이미지, 배경음악 구현

--- graduation_game.py
+++ graduation_game.py
@@ -3,6 +3,14 @@ import sys
 import time
 from tkinter import messagebox
 
+# 화면 크기 및 맵 크기 설정
+screen_BossGame_width = 800
+screen_BossGame_height = 800
+screen_width = 1000
+screen_height = 800
+map_width = 4000
+map_height = 800
+
 class Block:
     def __init__(self, x, y, width, height):
         self.rect = pygame.Rect(x, y, width, height)
@@ -41,11 +49,119 @@ class Item:
     def check_collision(self, player_rect):
         return self.rect.colliderect(player_rect)
 
-def runGame(screen_width, screen_height, map_width, map_height, screen, clock, player_image, background_stage1, assignment_image, thorn_image, hp_images, door_image, blocks, items, jump_sound, hit_sound, item_sound):
+def initGame():
+
+    pygame.init()
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    pygame.display.set_caption("졸업 게임")
+
+    # 플레이어 이미지 로드
+    player_image = pygame.image.load("player_image.png")
+    player_image = pygame.transform.scale(player_image, (100, 100))
+
+    # 배경 이미지 로드
+    background_stage = pygame.image.load("미래관.png")
+    background_stage = pygame.transform.scale(background_stage, (map_width + 1000, map_height))
+
+    # 장애물(과제) 이미지 로드
+    assignment_image = pygame.image.load("assignment_image.png")
+    assignment_image = pygame.transform.scale(assignment_image, (130, 130))
+
+    # 장애물(가시) 이미지 로드
+    thorn_image = pygame.image.load("thorn.png")
+    thorn_image = pygame.transform.scale(thorn_image, (1400, 100))
+
+    # HP 이미지 로드
+    hp_images = {
+        3: pygame.image.load("HP3.png").convert_alpha(),
+        2: pygame.image.load("HP2.png").convert_alpha(),
+        1: pygame.image.load("HP1.png").convert_alpha(),
+    }
+
+    door_image = pygame.image.load("door.png")
+    door_image = pygame.transform.scale(door_image, (400, 500))
+
+    # 블록 객체들 생성 및 리스트에 추가
+    blocks = [
+        Block(300, screen_height - 100, 100, 100),    # 폭탄 왼쪽 벽
+        Block(1100, screen_height - 100, 300, 100),   # 폭탄 오른쪽 벽 + 왼쪽 계단
+        Block(1400, screen_height - 200, 100, 200),   
+        Block(1500, screen_height - 300, 100, 300),
+        Block(3000, screen_height - 300, 100, 300),   # 오른쪽 계단
+        Block(3100, screen_height - 200, 100, 200),
+        Block(3200, screen_height - 100, 100, 100),
+        MovingBlock(1800, screen_height - 350, 200, 50, screen_height - 650, screen_height - 250, 5),    # 가시 위 첫번째 발판
+        MovingBlock(2200, screen_height - 500, 200, 50, screen_height - 650, screen_height - 250, -15),   # 가시 위 두번째 발판
+        MovingBlock(2600, screen_height - 350, 200, 50, screen_height - 650, screen_height - 250, 10),    # 가시 위 세번째 발판
+    ]
+              
+    # 아이템 객체 생성 및 리스트에 추가
+    items = [Item(1200, screen_height - 400, 250, 100, "HP1.png")]
+
+    # 점프 시 효과음 로드
+    jump_sound = pygame.mixer.Sound("jump.mp3")
+
+    # 피격 시 효과음 로드
+    hit_sound = pygame.mixer.Sound("hit.mp3")
+
+    # 아이템 획득 시 효과음 로드
+    item_sound = pygame.mixer.Sound("item.mp3")
+
+    # 게임 오버 시 효과음 로드
+    gameover_sound = pygame.mixer.Sound("gameover.mp3")
+
+    # 초기 배경 음악 로드 및 재생 (7초짜리)
+    pygame.mixer.music.load("initial_music.mp3")
+    pygame.mixer.music.play(1, 0.0)
+
+    # 대기하며 초기 음악이 재생 완료되길 기다림
+    '''while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(7)  # 대기시간 설정'''
+
+    # 초기 음악이 끝나면 다른 음악으로 교체 및 반복 재생
+    pygame.mixer.music.load("main_music.mp3")
+    pygame.mixer.music.play(-1)
+    
+    # 메인 루프
+    clock = pygame.time.Clock()
+    runGame(
+        screen,
+        clock,
+        player_image,
+        background_stage,
+        assignment_image,
+        thorn_image,
+        hp_images,
+        door_image,
+        blocks,
+        items,
+        jump_sound,
+        hit_sound,
+        item_sound,
+        gameover_sound
+        )
+
+def runGame(
+        screen,
+        clock,
+        player_image,
+        background_stage,
+        assignment_image,
+        thorn_image,
+        hp_images,
+        door_image,
+        blocks,
+        items,
+        jump_sound,
+        hit_sound,
+        item_sound,
+        gameover_sound
+        ):
+    
     # 플레이어 설정
     player_width = 100
     player_height = 100
-    player_x = 50
+    player_x = 3500
     player_y = screen_height - player_height
     player_speed = 10
     jump_height = 20
@@ -115,7 +231,7 @@ def runGame(screen_width, screen_height, map_width, map_height, screen, clock, p
         camera_x = player_x - screen_width/2.5
 
         # 배경 그리기 (맵의 배경)
-        screen.blit(background_stage1, (0 - camera_x - 400, 0))
+        screen.blit(background_stage, (0 - camera_x - 400, 0))
 
         # 플레이어를 카메라 위치에 따라 그리기
         screen.blit(player_image, (player_x - camera_x, player_y))
@@ -177,8 +293,26 @@ def runGame(screen_width, screen_height, map_width, map_height, screen, clock, p
             player_y < screen_height and
             player_y + player_height > screen_height - 400
         ):
+            # 게임 오버 시 음악 중단
+            pygame.mixer.music.stop()
+
             # 충돌 시 다음 스테이지로
-            player_hp -= 1
+            runBossGame(
+                screen,
+                clock,
+                player_image,
+                background_stage,
+                assignment_image,
+                thorn_image,
+                hp_images,
+                door_image,
+                blocks,
+                items,
+                jump_sound,
+                hit_sound,
+                item_sound,
+                gameover_sound
+            )
 
         # MovingBlock들의 move 메서드 호출
         for block in blocks:
@@ -247,6 +381,143 @@ def runGame(screen_width, screen_height, map_width, map_height, screen, clock, p
     # 게임 오버 시 음악 중단
     pygame.mixer.music.stop()
 
+    # gameover.mp3 재생
+    gameover_sound.play()
+
+    # 메시지 박스 표시
+    result = messagebox.askquestion("게임 종료", "다시하시겠습니까?")
+    if result == 'yes':
+        # gameover.mp3 중단
+        gameover_sound.stop()
+
+        # 음악 다시 재생
+        pygame.mixer.music.load("main_music.mp3")
+        pygame.mixer.music.play(-1)
+
+        # 아이템 위치 재설정
+        items[0].rect.x = 1200
+        items[0].rect.y = screen_height - 400
+        # 게임 재시작
+        runGame(
+        screen,
+        clock,
+        player_image,
+        background_stage,
+        assignment_image,
+        thorn_image,
+        hp_images,
+        door_image,
+        blocks,
+        items,
+        jump_sound,
+        hit_sound,
+        item_sound,
+        gameover_sound
+        )
+    else:
+        pygame.quit()
+        sys.exit()
+
+def runBossGame(
+        screen,
+        clock,
+        player_image,
+        background_stage,
+        assignment_image,
+        thorn_image,
+        hp_images,
+        door_image,
+        blocks,
+        items,
+        jump_sound,
+        hit_sound,
+        item_sound,
+        gameover_sound
+        ):
+
+    # 초기 음악이 끝나면 다른 음악으로 교체 및 반복 재생
+    pygame.mixer.music.load("boss_music.mp3")
+    pygame.mixer.music.play(-1)
+
+    # 보스 이미지 로드
+    boss_image = pygame.image.load("boss_image.png")
+    boss_image = pygame.transform.scale(boss_image, (800, 150))
+
+    # 배경 이미지 로드
+    boss_background_stage = pygame.image.load("코딩화면.png")
+    boss_background_stage = pygame.transform.scale(boss_background_stage, (1000, 800))
+
+    # 플레이어 설정
+    player_width = 100
+    player_height = 100
+    player_x = 50
+    player_y = screen_height - player_height
+    player_speed = 10
+    jump_height = 20
+    player_hp = 3
+
+    # 플레이어의 초기 속도 및 중력 설정
+    player_velocity_y = 0
+    gravity = 1.5
+
+    # 플레이어의 초기 무적 상태 및 무적 지속 시간 설정
+    invincible = False
+    invincible_duration = 0.5  # 무적 지속 시간 (초)
+    invincible_start_time = 0  # 무적이 시작된 시간
+
+    running = True
+    
+    while running and player_hp > 0:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        keys = pygame.key.get_pressed()
+
+        # 키 입력에 따라 플레이어의 위치 조정
+        if keys[pygame.K_LEFT] and player_x > 0:
+            player_x -= player_speed
+        if keys[pygame.K_RIGHT] and player_x < screen_BossGame_width + player_width:
+            player_x += player_speed
+        if keys[pygame.K_SPACE] and player_y == screen_BossGame_height - 100:
+            # jump 효과음 재생
+            jump_sound.play()
+            player_velocity_y = -jump_height
+
+        # 중력 적용
+        player_velocity_y += gravity
+        player_y += player_velocity_y
+
+        # 바닥에 닿으면 중력 초기화
+        if player_y > screen_BossGame_height - 100:
+            player_y = screen_BossGame_height - 100
+            player_velocity_y = 0
+
+        # 배경 그리기 (맵의 배경)
+        screen.blit(boss_background_stage, (0, 0))
+
+        # 플레이어를 카메라 위치에 따라 그리기
+        screen.blit(player_image, (player_x, player_y))
+
+        # 보스 그리기
+        screen.blit(boss_image, (100, 0))
+
+        # 무적 상태인 경우, 1초 동안은 무적을 유지
+        if invincible and time.time() - invincible_start_time > invincible_duration:
+            invincible = False
+                 
+        # HP 이미지 표시
+        if (player_hp >= 1): screen.blit(pygame.transform.scale(hp_images[player_hp], (150, 50)), (10, 10))
+
+        # 화면 업데이트
+        pygame.display.flip()
+
+        # FPS 설정
+        clock.tick(30)
+
+    # 게임 오버 시 음악 중단
+    pygame.mixer.music.stop()
+
     # 메시지 박스 표시
     result = messagebox.askquestion("게임 종료", "다시하시겠습니까?")
     if result == 'yes':
@@ -256,91 +527,27 @@ def runGame(screen_width, screen_height, map_width, map_height, screen, clock, p
 
         # 아이템 위치 재설정
         items[0].rect.x = 1200
-        items[0].rect.y = screen_height - 400
+        items[0].rect.y = screen_BossGame_height - 400
         # 게임 재시작
-        runGame(screen_width, screen_height, map_width, map_height, screen, clock, player_image, background_stage1, assignment_image, thorn_image, hp_images, door_image, blocks, items, jump_sound, hit_sound, item_sound)
+        runGame(
+        screen,
+        clock,
+        player_image,
+        background_stage,
+        assignment_image,
+        thorn_image,
+        hp_images,
+        door_image,
+        blocks,
+        items,
+        jump_sound,
+        hit_sound,
+        item_sound,
+        gameover_sound
+        )
     else:
         pygame.quit()
         sys.exit()
-
-def initGame():
-
-    # 화면 크기 및 맵 크기 설정
-    screen_width = 1000
-    screen_height = 800
-    map_width = 4000
-    map_height = 800
-
-    pygame.init()
-    screen = pygame.display.set_mode((screen_width, screen_height))
-    pygame.display.set_caption("졸업 게임")
-
-    # 플레이어 이미지 로드
-    player_image = pygame.image.load("player_image.png")
-    player_image = pygame.transform.scale(player_image, (100, 100))
-
-    # 배경 이미지 로드
-    background_stage1 = pygame.image.load("송민학교1.png")
-    background_stage1 = pygame.transform.scale(background_stage1, (map_width + 1000, map_height))
-
-    # 장애물(과제) 이미지 로드
-    assignment_image = pygame.image.load("assignment_image.png")
-    assignment_image = pygame.transform.scale(assignment_image, (130, 130))
-
-    # 장애물(가시) 이미지 로드
-    thorn_image = pygame.image.load("thorn.png")
-    thorn_image = pygame.transform.scale(thorn_image, (1400, 100))
-
-    # HP 이미지 로드
-    hp_images = {
-        3: pygame.image.load("HP3.png").convert_alpha(),
-        2: pygame.image.load("HP2.png").convert_alpha(),
-        1: pygame.image.load("HP1.png").convert_alpha(),
-    }
-
-    door_image = pygame.image.load("door.png")
-    door_image = pygame.transform.scale(door_image, (400, 500))
-
-    # 블록 객체들 생성 및 리스트에 추가
-    blocks = [
-        Block(300, screen_height - 100, 100, 100),    # 폭탄 왼쪽 벽
-        Block(1100, screen_height - 100, 300, 100),   # 폭탄 오른쪽 벽 + 왼쪽 계단
-        Block(1400, screen_height - 200, 100, 200),   
-        Block(1500, screen_height - 300, 100, 300),
-        Block(3000, screen_height - 300, 100, 300),   # 오른쪽 계단
-        Block(3100, screen_height - 200, 100, 200),
-        Block(3200, screen_height - 100, 100, 100),
-        MovingBlock(1800, screen_height - 350, 200, 50, screen_height - 650, screen_height - 250, 5),    # 가시 위 첫번째 발판
-        MovingBlock(2200, screen_height - 500, 200, 50, screen_height - 650, screen_height - 250, -15),   # 가시 위 두번째 발판
-        MovingBlock(2600, screen_height - 350, 200, 50, screen_height - 650, screen_height - 250, 10),    # 가시 위 세번째 발판
-    ]
-              
-    # 아이템 객체 생성 및 리스트에 추가
-    items = [Item(1200, screen_height - 400, 250, 100, "HP1.png")]
-
-    # 피격 시 효과음 로드
-    jump_sound = pygame.mixer.Sound("jump.mp3")
-
-    # 피격 시 효과음 로드
-    hit_sound = pygame.mixer.Sound("hit.mp3")
-
-    item_sound = pygame.mixer.Sound("item.mp3")
-
-    # 초기 배경 음악 로드 및 재생 (7초짜리)
-    '''pygame.mixer.music.load("initial_music.mp3")
-    pygame.mixer.music.play(1, 0.0)'''
-
-    # 대기하며 초기 음악이 재생 완료되길 기다림
-    while pygame.mixer.music.get_busy():
-        pygame.time.Clock().tick(30)  # 대기시간 설정
-
-    # 초기 음악이 끝나면 다른 음악으로 교체 및 반복 재생
-    pygame.mixer.music.load("main_music.mp3")
-    pygame.mixer.music.play(-1)
-    
-    # 메인 루프
-    clock = pygame.time.Clock()
-    runGame(screen_width, screen_height, map_width, map_height, screen, clock, player_image, background_stage1, assignment_image, thorn_image, hp_images, door_image, blocks, items, jump_sound, hit_sound, item_sound)
 
 if __name__ == '__main__':
     initGame()
